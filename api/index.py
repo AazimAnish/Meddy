de17190d7f8a69d8f1c8eb2caf38f21513782bc7
Jishnu M M <jishnumm4312@gmail.com>
2024-04-07 00:30:49 +0530

    fixed pymongo

--- api/index.py
+++ api/index.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask
+import json
+from bson import json_util
 from db import collection
 
 app = Flask(__name__)
@@ -6,10 +8,10 @@ app = Flask(__name__)
 
 @app.route("/api/python")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>hi, World!</p>"
 
 @app.route("/api/prescription")
 def prescription():
-    data = list(collection.find())  # Retrieve data from MongoDB collection
-    return jsonify(data)
+    data = collection.find() # Retrieve data from MongoDB collection
+    return json.loads(json_util.dumps(data))
 
