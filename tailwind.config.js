96772e0638ec02f344fc9a748ea219b7ae818de9
AazimAnish <aazimanish1@gmail.com>
2024-04-07 00:15:23 +0530

    feat: home page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        customBlue: {
+          900: '#020024',
+        },
+        customNavy: {
+          900: '#090979',
+        },
+        customCyan: {
+          400: '#00d4ff',
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
