1fb9a0936f0ae87c1b8e1ad0e067cc1021acb547
Jishnu M M <jishnumm4312@gmail.com>
2024-04-06 23:30:07 +0530

    added python backend code samples

--- api/index.py
+++ api/index.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+from db import collection
+
 app = Flask(__name__)
+
 
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/prescription")
+def prescription():
+    data = list(collection.find())  # Retrieve data from MongoDB collection
+    return jsonify(data)
+
