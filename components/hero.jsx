0e6678b700995771f597bd1f8032d242436f8e6c
AazimAnish <aazimanish1@gmail.com>
2024-04-07 10:00:57 +0530

    fix: node version upgraded

--- components/hero.jsx
+++ components/hero.jsx
@@ -65,7 +65,8 @@ export default function Hero() {
           <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
             <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
               <img
-                src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
+                // src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
+                src="../public/images/meddybot-front.svg"
                 alt="App screenshot"
                 width={2432}
                 height={1442}
