f53dabb0234a390d42f7563ceee516a7e98de2da
AazimAnish <aazimanish1@gmail.com>
2024-04-07 13:48:28 +0530

    changes

--- components/hero.jsx
+++ components/hero.jsx
@@ -66,15 +66,15 @@ export default function Hero() {
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32 h-screen">
           <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none h-full">
-            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4 h-full">
+            {/* <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4 h-full"> */}
               <img
                 src="../images/meddybotty.svg"
                 alt="App screenshot"
                 width={1216} // Adjusted width
                 height={721} // Adjusted height
-                className="w-full h-full rounded-md object-cover bg-transparent animate-move-up-down"
+                className="w-full h-full rounded-md object-cover bg-transparent animate-move-up-down -mt-20"
               />
-            </div>
+            {/* </div> */}
           </div>
         </div>
 
