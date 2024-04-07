aec3c3a9614386a729ba36392906341e00741f7e
Jishnu M M <jishnumm4312@gmail.com>
2024-04-08 00:15:46 +0530

    added few changes in backend

--- app/reg-patient/page.jsx
+++ app/reg-patient/page.jsx
@@ -195,6 +195,8 @@ export default function Profile() {
                   type="text"
                   name="phone"
                   id="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
                   autoComplete="phone"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
--- app/view-patients/page.jsx
+++ app/view-patients/page.jsx
@@ -7,8 +7,9 @@ export default function ViewPatients() {
 
     useEffect(() => {
         // Fetch data from the API
-        axios.get('/viewpatients')
+        axios.get('/api/get-appointments')
             .then(response => {
+                console.log(response.data)
                 // Assuming response.data contains an array of patient objects with firstname, lastname, and uhid properties
                 setPatients(response.data);
             })
@@ -18,15 +19,15 @@ export default function ViewPatients() {
     }, []);
 
     return (
-        <div className="bg-white p-10">
+        <div className="bg-white h-screen p-10">
             <h2 className="font-bold text-2xl">Today's Appointments</h2>
             <div className="p-20">
-                <ul role="list" className="divide-y divide-gray-100 bg-white">
-                    {patients.map((patient, index) => (
-                        <li key={index} className="flex justify-between gap-x-6 py-5">
+                <ul role="list" className="divide-y divide-gray-500 bg-white">
+                    {patients.length>0 && patients.map((patient, index) => (
+                        <li key={index} className="flex justify-between gap-x-6 py-5 cursor-pointer border-b-2 border-t-2 border-gray-400 hover:bg-gray-200 px-3">
                             <div className="flex min-w-0 gap-x-4">
                                 <div className="min-w-0 flex-auto">
-                                    <p className="text-sm font-semibold leading-6 text-gray-900">{`${patient.firstname} ${patient.lastname}`}</p>
+                                    <p className="text-sm font-semibold leading-6 text-gray-900">{`${patient.firstName} ${patient.lastName}`}</p>
                                     <p className="mt-1 truncate text-xs leading-5 text-gray-500">UHID: {patient.uhid}</p>
                                 </div>
                             </div>
