5dd98adf5d50a9d197311d986eda2bb17942694f
Mishal <mishal0404@gmail.com>
2024-04-08 10:06:07 +0530

    fixed post request

--- webhook/hello.py
+++ webhook/hello.py
@@ -275,7 +275,7 @@ def extract_audio():
                 audio_url = messenger.query_media_url(audio_id)
                 audio["mobile"] = mobile
                 audio["language"] = language
-                response = requests.post("https://meddy-rho.vercel.app/api/audios", json=audio, headers={'Content-Type': 'application/json'})
+                response = requests.post("https://meddy-rho.vercel.app/api/audios_add", json=audio, headers={'Content-Type': 'application/json'})
                 if response.status_code == 200:
                     print("Request successful:", response.content)
                 else:
