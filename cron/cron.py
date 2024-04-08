768885ea728c1d149250c16756c68db484e5f715
Mishal <mishal0404@gmail.com>
2024-04-08 11:31:51 +0530

    cron job

---
+++ cron/cron.py
@@ -0,0 +1,5 @@
+from heyoo import WhatsApp
+
+messenger = WhatsApp("EAAGL5dZCRyoEBOZBsZAf0R4d2rXwlddD76EykK8MifbFkDcvffrpNcBDHtAc6E7sP2mI2dwd2ESGdbSn0JJIhXu3uS39gRsnZCvZAGXdSTJVC2ZBKPTZCUDkMURmZBjWdLOdv1Ev2wiWuXOJO4d7Sms1cJZBZA1sQOjW7P207enDS0rVyRwXJSBzjQYc6XR5WC2HqQl0uykhl9ua1Rhv3j8kGX", phone_number_id="259164670622151")
+
+messenger.send_message("നിങ്ങളുടെ മരുന്നുകൾ കഴിക്കാൻ മറക്കരുത്", "918129953715")
